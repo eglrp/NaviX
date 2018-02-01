5e9531b31fd9ee09ed756e11647684371f3c4861
bailiqun <bailiqun@gmail.com>
2018-02-02 00:23:41 +0800

    $1

--- utils/udev/install.sh
+++ utils/udev/install.sh
@@ -2,10 +2,11 @@
 
 echo ""
 echo "This script copies a udev rule to /etc to facilitate bringing"
-echo "up the Hokuyo Lidar usb connection."
 echo ""
 
 echo "copy rules in udev"
 sudo cp ./96-hokuyo.rules /etc/udev/rules.d/
+sudo cp ./99-BCB.rules /etc/udev/rules.d/
+sudo cp ./99-turtlebot3-cdc.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
 sudo udevadm trigger
